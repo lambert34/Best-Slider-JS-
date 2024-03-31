b91d40d2d0e2d68a49a47db62828d45ffb6594ca
alexL <aleksandrlambert@gmail.com>
2024-03-31 23:36:42 +0700

    отловили необходимые элементы в дом

---
+++ js/app.js
@@ -0,0 +1,5 @@
+let nextDom = document.getElementById('next');
+let prevDom = document.getElementById('prev');
+let carouselDom = document.querySelector('.carousel');
+let listItemDom = document.querySelector('.carousel .list');
+let thumbnailDom = document.querySelector('.carousel .thumbnail');
